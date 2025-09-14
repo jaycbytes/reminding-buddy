1da7ecdcd08f49f09eca34397d965652c0348c5c
jayxor <castro_jayhangeer01@student.smc.edu>
2025-09-14 12:03:59 -0700

    Initial Commit

---
+++ study-companion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  study-companion
+//
+//  Created by Jay Castro on 9/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ study-companion/study_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_companionApp.swift
+//  study-companion
+//
+//  Created by Jay Castro on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct study_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
